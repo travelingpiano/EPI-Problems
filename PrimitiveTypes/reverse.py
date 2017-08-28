2d93ad574639d11cb8e9c6823793ea55f420af89
travelingpiano <lihsuanlu@gmail.com>
2017-08-28 15:45:49 -0700

    finish reverse digits

---
+++ PrimitiveTypes/reverse.py
@@ -0,0 +1,15 @@
+import math
+def reverse(x):
+    negative = False
+    if x < 0:
+        negative = ~negative
+        x = -x
+    remainder = x
+    reversed_x = 0
+    while remainder > 0:
+        reversed_x, remainder = reversed_x*10 + remainder%10, math.floor(remainder/10)
+    if negative:
+        reversed_x = -reversed_x
+    return reversed_x
+    
+print(reverse(-104))
